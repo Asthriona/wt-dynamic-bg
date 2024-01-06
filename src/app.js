b3f282dddb4953faa0d0aec5633974b4560cd96f
Makoto Kobayashi <im@asthriona.com>
2024-01-06 04:49:44 +0100

    Add error handling if no folder or images.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
+require("dotenv").config();
 const app = require("express")()
 const path = require("path");
 const fs = require("fs");
 
 function getImages() {
     const imgDir = path.join(__dirname, 'public', 'images');
+    if(!fs.existsSync(imgDir)) throw new Error('The folder "public/images" is not found, and yet required!');
     const images = fs.readdirSync(imgDir);
+    if(images.length === 0) throw new Error('No images found in the "public/images" folder.');
     const randomImage = images[Math.floor(Math.random() * images.length)]
     return path.join('public', 'images', randomImage);
 }
@@ -14,4 +17,4 @@ app.get("/random", (req,res) => {
     res.sendFile(path.join(__dirname, imagePath));
 })
 
-app.listen(3000, () => console.log("Running on port 3000"));
+app.listen(process.env.PORT, () => console.log(`App running on port ${process.env.PORT}`));
